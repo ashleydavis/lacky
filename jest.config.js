b54bf75a6101d813066929705d04ad71b390b678
Ash <ashley@codecapers.com.au>
2025-10-27 08:04:06 +1000

    Fixed Jest configuration for Bun.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
-module.exports = {
+export default {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testMatch: ['**/test/**/*.test.ts'],
+    modulePathIgnorePatterns: [
+        "dist",
+        "build",
+        "bin",
+    ],
     moduleNameMapper: {
         '^ora$': '<rootDir>/src/__mocks__/ora.js',
         '^(\\.{1,2}/.*)\\.js$': '$1',
